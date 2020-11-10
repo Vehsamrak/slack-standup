6c010ef44d109efe00e8480301eb4aa4d7cb9f46
Petr Karmashev <karmashev@yandex-team.ru>
2020-11-10 19:52:23 +0300

    #3 Bold formatting for questions

--- internal/app/meeting/Meeting.go
+++ internal/app/meeting/Meeting.go
@@ -12,13 +12,13 @@ func (meeting Meeting) Create(thread *chatPostMessage.Thread) *Meeting {
 }
 
 func (meeting *Meeting) QuestionPrevious() string {
-	return "Удалось выполнить предыдущий план?"
+	return "*Удалось выполнить предыдущий план?*"
 }
 
 func (meeting *Meeting) QuestionToday() string {
-	return "Что планируешь сделать сегодня?"
+	return "*Что планируешь сделать сегодня?*"
 }
 
 func (meeting *Meeting) QuestionBlock() string {
-	return "Кто и чем может тебе в этом помочь?"
+	return "*Кто и чем может тебе в этом помочь?*"
 }
